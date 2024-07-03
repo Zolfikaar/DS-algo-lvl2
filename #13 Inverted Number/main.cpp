738fd6117d1346605723080a35348549bf9a66bf
Tholfikar <fukar.work@gmail.com>
2024-07-03 12:30:38 +0300

    Complete Challange #13

---
+++ #13 Inverted Number/main.cpp
@@ -0,0 +1,37 @@
+// This solution is O(n^2) Time Complexity, i need to make it O(n) by not using nested loop
+
+#include <iostream>
+using namespace std;
+int ReadPositiveNumber(string Message)
+{
+  int Number = 0;
+  do
+  {
+    cout << Message << endl;
+    cin >> Number;
+  } while (Number <= 0);
+  return Number;
+}
+
+void PrintSingleNumber(int num)
+{
+  for (int j = 1; j <= num; j++)
+  {
+    cout << num;
+  }
+
+  cout << "\n";
+}
+
+void PrintInvertedNumber(int Number)
+{
+  for (int i = 1; i <= Number; i++)
+  {
+    PrintSingleNumber(i);
+  }
+}
+
+int main()
+{
+  PrintInvertedNumber(ReadPositiveNumber("Please Enter a Number?"));
+}
