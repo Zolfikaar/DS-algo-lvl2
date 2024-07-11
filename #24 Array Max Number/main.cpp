678dba85f70e1d8e6d922a92be55059a575d2bca
Tholfikar <fukar.work@gmail.com>
2024-07-11 10:09:42 +0300

    Complete challange #24

---
+++ #24 Array Max Number/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int RandomNumber(int from, int to)
+{
+  int rand_char = rand() % (to - from + 1) + from;
+
+  return rand_char;
+}
+
+int ReadArrayLength()
+{
+  int arrLength;
+
+  cout << "\nEnter the number of array length: \n";
+
+  cin >> arrLength;
+
+  return arrLength;
+}
+
+void PrintArray(int arrLength)
+{
+  int arr[arrLength];
+
+  for (int i = 0; i < arrLength; i++)
+  {
+    arr[i] = RandomNumber(1, 100);
+  }
+
+  cout << "Array Elements: ";
+
+  for (int j = 0; j < arrLength; j++)
+  {
+    cout << arr[j] << " ";
+  }
+  cout << "\n";
+
+  int max = 0;
+
+  for (int i = 0; i < arrLength; i++)
+  {
+    if (arr[i] > max)
+    {
+      max = arr[i];
+    }
+  }
+
+  cout << "Max Number is: " << max;
+}
+
+int main()
+{
+  // Seed the random number generator in c++, call it only once.
+  srand((unsigned)time(NULL));
+
+  int arrLength = ReadArrayLength();
+
+  PrintArray(arrLength);
+
+  return 0;
+}
