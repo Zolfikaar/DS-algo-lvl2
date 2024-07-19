c1fe2b3cbf893404905e6dd73dd6a6b0d74b9e9e
Tholfikar <fukar.work@gmail.com>
2024-07-19 12:55:43 +0300

    Complete #34 challange

---
+++ #34 Return number index in array/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int RandomNumber(int from, int to)
+{
+  int rand_char = rand() % (to - from + 1) + from;
+
+  return rand_char;
+}
+
+int ReadArrayLength()
+{
+  int arrLength;
+
+  cout << "\nEnter array length: \n";
+
+  cin >> arrLength;
+
+  return arrLength;
+}
+
+int ReadNumberToLookFor()
+{
+  int number;
+
+  cout << "Please enter a number to search for: ";
+
+  cin >> number;
+
+  return number;
+}
+
+void PrintArray(int arrLength)
+{
+  int arr[arrLength];
+
+  for (int i = 0; i < arrLength; i++)
+  {
+    arr[i] = RandomNumber(1, 100);
+  }
+
+  cout << "Array Elements:\n";
+
+  for (int j = 0; j < arrLength; j++)
+  {
+    cout << arr[j] << " ";
+  }
+
+  cout << "\n";
+
+  int number = ReadNumberToLookFor();
+  int numberPosition, numberOrder;
+  bool numberFound = false;
+
+  for (int k = 0; k < arrLength; k++)
+  {
+    if (arr[k] == number)
+    {
+      numberPosition = k;
+      numberOrder = k + 1;
+      numberFound = true;
+    }
+  }
+
+  if (numberFound)
+  {
+    cout << "Number you are looking for: " << number << "\n";
+    cout << "The Number found at position: " << numberPosition << "\n";
+    cout << "The Number order is: " << numberOrder;
+  }
+  else
+  {
+    cout << "Number you are looking for: " << number << "\n";
+    cout << "The number is not found :-(";
+  }
+}
+
+int main()
+{
+  // Seed the random number generator in c++, call it only once.
+  srand((unsigned)time(NULL));
+
+  int arrLength = ReadArrayLength();
+
+  PrintArray(arrLength);
+
+  return 0;
+}
